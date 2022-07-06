e1ebce77afc418fd59e1da9b59f53f98879e840c
Johnny Hsu <a209328641@gmail.com>
2022-07-06 17:27:29 +0800

    created cart modal and backdrop

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+
+function Cart(props) {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "c1", name: "Sushi", amount: 2, price: 300 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal>
+      {cartItems}
+      <div className={classes.total}>
+        <span>Total amount</span>
+        <span>NTD 300</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+}
+
+export default Cart;
